c223fbc0fb3c49c2346502ca3c8d1c06ff00d388
blunatic <brendanluna1@gmail.com>
2014-04-26 21:16:42 -0700

    first commit x 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwell::Application.config.secret_key_base = '1c6d1388157679b31d54e1c9b5eb882cd37c8c1ec75a166654f7692bfeb944d63d801b0b510598b8d9811137df08abb9783bc87d8bf3450c393bf598b6f6b96e'
