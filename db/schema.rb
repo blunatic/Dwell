21644ba75443a493bafeb8f25d577efda14d2a65
blunatic <brendanluna1@gmail.com>
2014-04-30 16:43:39 -0700

    initialize household model db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427034847) do
+ActiveRecord::Schema.define(version: 20140430233609) do
+
+  create_table "households", force: true do |t|
+    t.string   "household_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
