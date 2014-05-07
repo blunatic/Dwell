8b3f1c39d67f2d07f20d1ee8bc836f0bdd42b5ac
blunatic <brendanluna1@gmail.com>
2014-05-07 13:38:47 -0700

    Added favicon icon (for browser tabs)

--- db/schema.rb
+++ db/schema.rb
@@ -46,29 +46,6 @@ ActiveRecord::Schema.define(version: 20140507081610) do
     t.integer  "household_id"
   end
 
-  create_table "fullcalendar_engine_event_series", force: true do |t|
-    t.integer  "frequency",  default: 1
-    t.string   "period",     default: "monthly"
-    t.datetime "starttime"
-    t.datetime "endtime"
-    t.boolean  "all_day",    default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "fullcalendar_engine_events", force: true do |t|
-    t.string   "title"
-    t.datetime "starttime"
-    t.datetime "endtime"
-    t.boolean  "all_day",         default: false
-    t.text     "description"
-    t.integer  "event_series_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "fullcalendar_engine_events", ["event_series_id"], name: "index_fullcalendar_engine_events_on_event_series_id"
-
   create_table "households", force: true do |t|
     t.string   "name"
     t.string   "password"
@@ -77,23 +54,23 @@ ActiveRecord::Schema.define(version: 20140507081610) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "household_id",           limit: 255
     t.string   "first_name"
     t.string   "last_name"
     t.string   "household_password"
     t.string   "household_name"
+    t.integer  "household_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
