71c93c59979a8675a41c0ef3adc1012511c53740
blunatic <brendanluna1@gmail.com>
2014-05-14 13:47:24 -0700

    Fixed Venmo payments, works now after user authenticates via Venmo login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,11 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
- respond_to :json
+  respond_to :json
 
   def index
     if !params[:access_token].nil?
       flash[:success] = "Venmo successfully authenticated!"
     end
-
   end
 
   def confirm_payment
@@ -14,18 +13,24 @@ class DashboardController < ApplicationController
   end
 
   def make_payment_path
-  	if !params[:access_token].nil?
+  	 
   		uri = URI.parse("https://api.venmo.com/v1/payments")
   		http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   		request = Net::HTTP::Post.new(uri.request_uri)
-  		request.set_form_data({"access_token" => params[:access_token], "email" => params[:payment_target],
-  		 "amount" => params[:payment_amount], "note" => params[:payment_note]} )
+  		request.set_form_data("access_token" => params[:access_token], "email" => params[:payment_target],
+  		 "amount" => params[:payment_amount], "note" => params[:payment_note] )
 
   		response = http.request(request)
-      render json: response.body
-     	end
+
+      if(response["status"] == "pending" || "settled")
+      redirect_to({ action: 'index' }, alert: "Payment complete!")
+      else
+        redirect_to :back
+      end
+
+     	
   end
 
   helper_method :make_payment_path
