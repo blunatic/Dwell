b50770997ad83a5bf160ca903370e4ddd29befd7
blunatic <brendanluna1@gmail.com>
2014-05-13 18:06:42 -0700

    Changing precompile assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ module Dwell
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w(*.png + *.jpg *.jpeg *.gif)
-
+    config.serve_static_assets = true
+    
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
